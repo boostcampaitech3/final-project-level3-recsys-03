4a7621af5ee2dab56203479f2d0adc6f09d239ef
kysuk05 <ravage13@naver.com>
2022-06-07 05:56:53 +0000

    수정 Image upload 기능
    현재 crop된 이미지가 업로드 되는 것이 아닌 사용자가 업로드한 이미지가 그대로 업로드 되는 것을 확인하여 수정하였습니다.

--- Frontend/demo.py
+++ Frontend/demo.py
@@ -23,6 +23,13 @@ if args.docker=='False':
 elif args.docker=='True':
     backend = "http://fastapit:8000/"
 
+def convert_pil_image_to_byte_array(img):
+    img_byte_array = io.BytesIO()
+    img.save(img_byte_array, format='JPEG', subsampling=0, quality=100)
+    img_byte_array = img_byte_array.getvalue()
+    return img_byte_array
+
+
 def getData(img_file, server_url = backend + 'getSimilarFashion'):
     # m = MultipartEncoder(fields={"category": category, "file": ("filename", img_file, "image/jpeg")}) # category 추가시 충돌 버그
     m = MultipartEncoder(fields={"file": ("filename", img_file, "image/jpeg")})
@@ -64,7 +71,8 @@ if img_file:
 if st.button("업로드 완료"):
     if cropped_img :
         with st.spinner('로딩중...'):
-            img_np_array = img_file
+            cropped_img_bytearray = convert_pil_image_to_byte_array(cropped_img)
+            img_np_array = cropped_img_bytearray
             # ConnectionError: HTTPConnectionPool 방지
             try:
                 result = getData(img_np_array) 
