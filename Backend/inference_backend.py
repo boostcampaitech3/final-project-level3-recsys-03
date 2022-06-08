5ca6e46d86c27c8b3cbfdb64d88d0b4a9fbb5c56
kysuk05 <ravage13@naver.com>
2022-06-08 11:11:41 +0000

    Fix: model 리턴값에 맞게 수정
    수정된 model 리턴값에 맞게 backend 코드를 변경하였습니다.

--- Backend/inference_backend.py
+++ Backend/inference_backend.py
@@ -13,12 +13,14 @@ def get_similar_fashion_model(image): # 이미지 찾기
     img_URL_list = csv[image_IDs]['image_URL'] # CSV 파일에서 해당 이미지의 URL을 불러온다
     '''
     conf = config.parse_args()
-    img_list = []
     # 임시로 만든 img_URL_list
-    category,img_URL_list = inference.main(conf)
-    # similar_fashion_list = img_URL_list
-    #return {"image0" : similar_fashion_list[0], "image1" : similar_fashion_list[1], "image2" : similar_fashion_list[2], "image3" : similar_fashion_list[3]}
-    return (category,img_URL_list)
+    category, topk_title, topk_price, topk_item_url, topk_img_url = inference.main(conf)
+    print(category)
+    print(topk_title)
+    print(topk_price)
+    print(topk_item_url)
+    print(topk_img_url)
+    return (category, topk_title, topk_price, topk_item_url, topk_img_url)
 
 def get_category_model(image): # 카테고리 찾기
     '''
